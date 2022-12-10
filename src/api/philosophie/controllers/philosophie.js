c2e738d5f053c8ca975d9fb9368ba928419e9e3c
Hendrik Schmidt <hendriknas@gmail.com>
2022-12-10 17:34:29 +0100

    update schemas

---
+++ src/api/philosophie/controllers/philosophie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * philosophie controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::philosophie.philosophie');
