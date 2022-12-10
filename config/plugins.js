59708fd295e1874d739f74afd3bc5eef93931726
Hendrik Schmidt <hendriknas@gmail.com>
2022-12-10 11:56:57 +0100

    Create plugins.js

---
+++ config/plugins.js
@@ -0,0 +1,22 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      breakpoints: {
+        xlarge: 1500,
+        large: 1000,
+        medium: 750,
+        small: 500,
+      },
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
