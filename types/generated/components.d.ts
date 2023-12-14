052fdcd98257b18f81443cc7c63af42f40134f78
Hendrik Schmidt <hendriknas@gmail.com>
2023-12-14 14:03:58 +0100

    use new strapi admin

---
+++ types/generated/components.d.ts
@@ -0,0 +1,24 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface PersonalBiographie extends Schema.Component {
+  collectionName: 'components_personal_biographies';
+  info: {
+    displayName: 'Biographie';
+    icon: 'address-card';
+    description: '';
+  };
+  attributes: {
+    Nom: Attribute.String & Attribute.Required;
+    Position: Attribute.String & Attribute.Required;
+    Description: Attribute.RichText & Attribute.Required;
+    Portrait: Attribute.Media;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'personal.biographie': PersonalBiographie;
+    }
+  }
+}
